1d1e7594922f68c09f35ea3349b0e22e3d5faf5a
fangyinrui <fangyinrui@58.com>
2021-07-19 20:19:07 +0800

    单调栈-每日温度

---
+++ src/stack/N_739_dailyTemperature.java
@@ -0,0 +1,19 @@
+package stack;
+
+/**
+ * 给你一个数组 T，这个数组存放的是近几天的天气气温，你返回一个等长的数组，
+ * 计算：对于每一天，你还要至少等多少天才能等到一个更暖和的气温；如果等不到那一天，填 0。
+ */
+public class N_739_dailyTemperature {
+
+    /*
+    这个问题本质上还是寻找数组中下一个更大数，只不过结果不是下一个更大数是多少，而是距当前元素的距离
+     */
+    private static int[] solution(int[] temp) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
