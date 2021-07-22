d34fa52d8a72d8a1c16260a334d2c503cad3c898
fangyinrui <fangyinrui@58.com>
2021-07-22 19:53:37 +0800

    删除重复字符，字典序最小 update

--- src/stack/N_316_removeDup.java
+++ src/stack/N_316_removeDup.java
@@ -1,5 +1,7 @@
 package stack;
 
+import java.util.Stack;
+
 /**
  * 给定只包含小写字母的字符串，返回去重后字典序最小的字符串，要求不能打乱字符串中字符相对顺序
  */
@@ -11,13 +13,43 @@ public class N_316_removeDup {
     但本题要求返回字典序最小的结果，一般思路对去重后结果排序，但又要求了不能打乱相对顺序，似乎有些矛盾
     1 去重。 使用数组记录每个字符是否出现
     2 字典序最小&保持相对顺序。 使用单调栈
-    3 只出现一次字符不可删除。 记录每个字符出现次数
+    3 只出现一次字符不可删除。 记录数组每个字符出现次数，当后面没有该字符，不再pop
      */
-    private static void solution(String str) {
+    private static String solution(String str) {
+        boolean[] inStack = new boolean[256];
+        int[] cnt = new int[256];
+        for (char c : str.toCharArray()) {
+            cnt[c]++;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            //每遍历字符，cnt--
+            cnt[c]--;
+            if(inStack[c])
+                continue;
 
+            while (!stack.isEmpty() && stack.peek() > c) {
+                //如果后面字符不再存在，则停止pop
+                if(cnt[stack.peek()]==0)
+                    break;
+                char e = stack.pop();
+                inStack[e] = false;
+            }
+            stack.push(c);
+            inStack[c] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
     }
 
     public static void main(String[] args) {
-
+        String res = solution("bcabc");
+        System.out.println(res);
+        String res2 = solution("cbacdcbc");
+        System.out.println(res2);
     }
 }
