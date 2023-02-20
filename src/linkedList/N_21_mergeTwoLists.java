97f91f68f12bf38562930fab72cf90439daca03a
fangyinrui <fangyinrui@58.com>
2023-02-20 11:39:33 +0800

    N_21_合并两个有序链表

---
+++ src/linkedList/N_21_mergeTwoLists.java
@@ -0,0 +1,52 @@
+package linkedList;
+
+import base.ListNode;
+
+/**
+ * 合并两个有序列表
+ */
+public class N_21_mergeTwoLists {
+
+    //分治方法
+    private static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
+    }
+
+    // 正常遍历方法
+    private static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+
+        if (l1 != null) {
+            p.next = l1;
+        }
+        if (l2 != null) {
+            p.next = l2;
+        }
+
+        return dummy.next;
+    }
+
+}
